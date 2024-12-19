3d845a96438c5868e075941b097f1f37e3f1ab34
Ayush Wardhan (Second Account) <45749740+xshayu@users.noreply.github.com>
2024-12-19 18:39:27 +0000

    [FEAT] Implemented assignment

---
+++ crio-xlogin/src/App.tsx
@@ -0,0 +1,59 @@
+import React, { useState, FormEvent } from 'react';
+
+const XLogin = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const username = formData.get('username') as string;
+    const password = formData.get('password') as string;
+
+    console.log({username, password});
+    
+    if (username === 'user' && password === 'password') {
+      console.log('logged in');
+      setLoggedIn(true);
+    }
+  };
+
+  return (
+    <main style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100dvw'}}>
+    {
+      !loggedIn ?
+      <form onSubmit={handleSubmit}>
+        <h1>Login Page</h1>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              required
+            />
+          </div>
+    
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+            />
+          </div>
+    
+          <button type="submit">Submit</button>
+    
+        </form>
+      :
+      <>
+        <h1>Welcome</h1>
+        <p>Welcome, user!</p>
+      </>
+    }
+    </main>
+  );
+};
+
+export default XLogin;
